c2581b43c590463b657180278d0dfcac050645c9
Christopher Welborn <cj@welbornproductions.net>
2013-03-03 19:25:38 -0600

    fixed unresolved import for get_user_agent

--- home/views.py
+++ home/views.py
@@ -3,22 +3,20 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 # User-Agent helper...
-from django_user_agents.utils import get_user_agent #@Stupid Aptana
-
-
+from django_user_agents.utils import get_user_agent #@UnresolvedImport
 
 def index(request):
     """ serve up main site """
     # get user agent
     user_agent = get_user_agent(request)
-    sbrowser = user_agent.browser.family.lower()
+    browser_name = user_agent.browser.family.lower()
     
     # get css to use (non-IE, it's handled in the template)...
-    if "opera" in sbrowser:
+    if "opera" in browser_name:
         extra_style_link = "/static/css/template-opera.css"
-    elif "firefox" in sbrowser:
+    elif "firefox" in browser_name:
         extra_style_link = "/static/css/template-gecko.css"
-    elif "chrome" in sbrowser:
+    elif "chrome" in browser_name:
         extra_style_link = "/static/css/template-webkit.css"
     else:
         extra_style_link = False
@@ -27,7 +25,7 @@ def index(request):
     
     # initialize main template
     tmp_main = loader.get_template('home/main.html')
-    default_content = sbrowser
+    default_content = browser_name
     # load variables into template
     c_main = Context({'extra_style_link': extra_style_link,
                       'default_content': default_content})
