6c5c0c49d3ebd4c30a31d50d412daa46b000ba86
Christopher Welborn <cj@welbornprod.com>
2014-01-16 18:02:28 -0600

    Bumped version number, after pyval was added.

--- wp_main/settings.py
+++ wp_main/settings.py
@@ -6,7 +6,7 @@
 #  2.0 is considered a new beginning because the project
 #  is moving towards a backward-incompatible state,
 #  where all python 2 'hacks' will be removed.)
-WPVERSION = '2.0.3'
+WPVERSION = '2.0.4'
 
 # file/path (path joining)
 import os.path
