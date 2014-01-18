7aa022c7e48b8ea9017867f5157a32b4b3c0a1e3
Christopher Welborn <cj@welbornprod.com>
2014-01-18 14:08:03 -0600

    Added features to viewer. Disables files in menu.
    Added javascript to disable current file in the
    file menu. Clicking the current file's name will
    do nothing, and it's greyed out. When a new file
    is loaded, the menu is updated to do the same
    with the new 'current file'.

--- viewer/static/js/wpviewer.js
+++ viewer/static/js/wpviewer.js
@@ -1,3 +1,111 @@
+/* Welborn Productions - Viewer
+   Handles file-viewer actions on the client side.
+   - Christopher Welborn 2013 -desc added in 2014 :)
+*/
+
+// Store current relative filename here. The template sends it on the first
+// page load, and then it is set with JS when doing ajax calls. 
+var wpviewer = {
+    // add a menu item to the vertical menu.
+    add_menu_item : function (menuname, menufilename, disabledval) {
+        /* menuname      : short file name (file.py)
+           menufilename  : relative file name (/static/blah/file.py)
+           disabledval   : 'true', or 'false' (sent to onclick: view_file() )
+                           must be string value. default is 'false'
+        */
+        if (!disabledval) { disabledval = 'false'; }
+
+        var menulink = document.createElement('a');
+       
+        $(menulink).attr('href', 'javascript: void(0);');
+        $(menulink).attr('class', 'vertical-menu-link');
+        // add child list element/name
+        var menuitem = document.createElement('li');
+        $(menuitem).addClass('vertical-menu-item');
+        if (disabledval == 'true') {
+            $(menuitem).addClass('vertical-menu-item-disabled');
+        }
+        // add child span element/name
+        var menutext = document.createElement('span');
+        $(menutext).addClass('vertical-menu-text');
+        // make text a child of list, which is a child of the link.
+        $(menutext).text(menuname);
+        $(menuitem).append(menutext);
+        $(menulink).append(menuitem);
+
+        $(menulink).attr('onclick', 'javascript: ' + 
+                                    'view_file(' +
+                                        '\'' + menufilename + '\',' + 
+                                        disabledval + 
+                                    ');');
+        // needs disabled class?
+        if (disabledval == 'true') {
+            $(menulink).addClass('vertical-menu-item-disabled');
+        }
+        // add this menu item
+        $("#file-menu-items").append(menulink);        
+    },
+
+    // current relative path for file. (/static/file.py)
+    current_file : '',
+    // current short name (file.py)
+    current_name : '',
+
+    enable_link : function (linkelem, enabled) {
+        /* Make a menu item enabled/disabled */
+        var menutarget = $(linkelem).attr('onclick');
+        var linkitem = $(linkelem).children();
+        if (!menutarget) { 
+            console.log('can\'t find href for element: ' + linkelem);
+            return false;
+        }
+        if (enabled) {
+            // ensure this link is 'enabled'
+            if (menutarget.indexOf('false') < 0) {
+                // change 'false' to 'true' because view_file(filename, true)
+                // means 'do not load this file', cancel = true
+                $(linkelem).attr('onclick', menutarget.replace('true', 'false'));
+                $(linkelem).removeClass('vertical-menu-item-disabled');
+                $(linkitem).removeClass('vertical-menu-item-disabled');
+                return true;
+            } else {
+                // already enabled.
+                return true;
+            }
+
+        } else {
+            // ensure this link is 'disabled' (true means disabled)
+            if (menutarget.indexOf('true') < 0) {
+                $(linkelem).attr('onclick', menutarget.replace('false', 'true'));
+                $(linkelem).addClass('vertical-menu-item-disabled');
+                $(linkitem).addClass('vertical-menu-item-disabled');
+                return true;
+            } else {
+                // already disabled.
+                return true;
+            }
+        }
+    },
+
+    set_current_file : function (filename) {
+        if (filename) {
+            if (filename[0] == '/') {
+                wpviewer.current_file = filename;
+            } else {
+                wpviewer.current_file = '/' + filename;
+            }
+            // set shortname for file.
+            var nameparts = wpviewer.current_file.split('/');
+            wpviewer.current_name = nameparts[nameparts.length -1];
+            
+        }
+    }
+
+};
+
+/* This all needs to be contained in 'wpviewer', but i'll leave that for
+   another day.
+*/
 function change_content(s) {
     /* Change ace editor content, move selection to start. */
     wp_content.setValue(s);
@@ -12,9 +120,9 @@ function load_file_data (xhrdata) {
     if (file_info.status == 'error') {
         var errmessage = file_info.message;
         if (errmessage) {
-            $("#file-content-box").html("<span>" + errmessage + "</span>");
+            $('#file-content-box').html('<span>' + errmessage + '</span>');
         } else {
-            $("#file-content-box").html("<span>Sorry, an unknown error occurred.</span>");
+            $('#file-content-box').html('<span>Sorry, an unknown error occurred.</span>');
         }
         return;
     }
@@ -30,46 +138,70 @@ function load_file_data (xhrdata) {
 
     } 
     else {
-        $("#file-content-box").html("<span>Sorry, no content in this file...</span>");
+        $('#file-content-box').html('<span>Sorry, no content in this file...</span>');
         return;
     }
 
     /* Header for projects ...pythons None equates to null in JS. */
     if (file_info.project_alias !== null && file_info.project_name !== null) {
-        $("#project-title-name").html(file_info.project_name);
-        $("#project-link").click( function () { wptools.navigateto("/projects/" + file_info.project_alias); });
-        if (file_info.project_alias) { $("#project-info").fadeIn(); }
+        $('#project-title-name').html(file_info.project_name);
+        $('#project-link').click( function () { wptools.navigateto('/projects/' + file_info.project_alias); });
+        if (file_info.project_alias) { $('#project-info').fadeIn(); }
     // Header for miscobj.
     } else if (file_info.misc_alias !== null && file_info.misc_name !== null) {
-        $("#project-title-name").html(file_info.misc_name);
-        $("#project-link").click( function () { wptools.navigateto("/misc/" + file_info.misc_alias); });
-        if (file_info.misc_alias) { $("#project-info").fadeIn(); }
+        $('#project-title-name').html(file_info.misc_name);
+        $('#project-link').click( function () { wptools.navigateto('/misc/' + file_info.misc_alias); });
+        if (file_info.misc_alias) { $('#project-info').fadeIn(); }
     }
     // File info
     if (file_info.file_short && file_info.static_path) {
-        $("#viewer-filename").html(file_info.file_short);
-        $("#file-link").click( function () { wptools.navigateto("/dl/" + file_info.static_path ); });
-        if (file_info.file_short) { $("#file-info").fadeIn(); }
+        $('#viewer-filename').html(file_info.file_short);
+        $('#file-link').click( function () { wptools.navigateto('/dl/' + file_info.static_path ); });
+        if (file_info.file_short) { $('#file-info').fadeIn(); }
+        // set current working filename.
+        wpviewer.set_current_file(file_info.static_path);
     }
         
     // Menu builder
     var menu_items = file_info.menu_items;
     if (menu_items) {
         var menu_length = menu_items.length;
-        var existing_items = $(".vertical-menu-item");
+        var existing_items = $('.vertical-menu-item');
         var existing_length = existing_items.length;
-        // only build the menu once...
+        var menufilename;
+        var menuname;
+        // Build the menu from scratch if this is the first load.
         if (existing_length === 0 && menu_length > 0 ) {
             $.each(menu_items, function () {
-                $("#file-menu-items").append("<a href='javascript: view_file(\"" + this[0].replace(/[ ]/g, '/') + "\");'>" +
-                                             "<li class='vertical-menu-item'>" + 
-                                             "<span class='vertical-menu-text'>" + this[1] + "</span></li></a>");
+                menufilename = this[0].replace(/[ ]/g, '/');
+                menuname = this[1];
+                var disabledval = 'false';
+                // Disable current file in menu.
+                if (menufilename == wpviewer.current_file) {
+                    // this item is disabled.
+                    disabledval = 'true';
+                }
+                wpviewer.add_menu_item(menuname, menufilename, disabledval);
             });
-            $("#file-menu").fadeIn();
+            $('#file-menu').fadeIn();
+        } else {
+            // Re-mark current file (disable current file in menu)
+            $.each($(".vertical-menu-link"), function () {
+                // get name from this link.
+                menuname = $(this).children().text();
+                if (menuname == wpviewer.current_name) {
+                    // fix onclick for view_file().
+                    wpviewer.enable_link(this, false);
+                } else {
+                    // ensure this item is enabled.
+                    wpviewer.enable_link(this, true);
+                }
+            });
         }
     }
         
 }
+
 
 // setup initial ace editor
 function setup_ace (initial_filename) {
@@ -96,38 +228,47 @@ function setup_ace (initial_filename) {
 
 // update floater message and size/position
 function update_loading_msg (message) {
-    $("#loading-msg").html(message);
-    wptools.center_element("#floater");
-    var floater = $("#floater");
+    $('#floater-msg').html(message);
+    wptools.center_element('#floater');
+    var floater = $('#floater');
     var scrollpos = $(this).scrollTop();
     floater.css({'top': (scrollpos + 200) + 'px'});
     
-    $("#floater").fadeIn();
+    $('#floater').fadeIn();
 }
 
-function view_file (filename) {
+function view_file (filename, cancel) {
+    if (!filename) {
+        return false;
+    }
+
+    if (cancel) {
+        return false;
+    }
+
+    // JSON data to send...
 	var filedata = { file: filename };
 
 	// change the loading message for proper file name.
-	update_loading_msg("<span>Loading file: " + filename + "...</span>");
+	update_loading_msg('<span>Loading file: ' + wpviewer.current_file + '...</span>');
 
-	//$("#project-info").fadeOut();
-	$("#file-info").fadeOut();
+	//$('#project-info').fadeOut();
+	$('#file-info').fadeOut();
 	
 	$.ajax({
-		type: "post",
-		contentType: "application/json",
-		url: "/view/file/",
+		type: 'post',
+		contentType: 'application/json',
+		url: '/view/file/',
 		data: JSON.stringify(filedata),
-		dataType: "json",
+		dataType: 'json',
 		failure: function (xhr, status, errorthrown) {
-			console.log("failure: " + status);
+			console.log('failure: ' + status);
 		},
 		complete: function (xhr, status) {
 						
 			// handle errors...
 			if (status == 'error') {
-				$("#file-content-box").html("<span class='B'>Sorry, either that file doesn't exist, or there was an error processing it.</span>");
+				$('#file-content-box').html('<span class=\'B\'>Sorry, either that file doesn\'t exist, or there was an error processing it.</span>');
 				console.log('wp-error response: ' + xhr.responseText);
 			} 
 			else {
@@ -135,7 +276,7 @@ function view_file (filename) {
 				load_file_data(xhr);
 			}
 			// done loading success or error
-			$("#floater").fadeOut();
+			$('#floater').fadeOut();
 		},
 		status: {
 			404: function () { console.log('PAGE NOT FOUND!'); },
