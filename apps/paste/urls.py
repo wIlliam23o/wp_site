f335ba87b91ded28a6fc4c8fdace03ee5813d17a
Christopher Welborn <cj@welbornprod.com>
2014-04-29 20:58:57 -0500

    Reformatted a couple urls.py files.

--- apps/paste/urls.py
+++ apps/paste/urls.py
@@ -2,21 +2,22 @@ from django.conf.urls import patterns, url
 
 from apps.paste import views
 # Patterns for pastebin app.
-urlpatterns = patterns('',
-                       # paste main
-                       url(r'^/?$', views.view_index),
-                       # public api submit
-                       url(r'/api/submit/?', views.submit_public),
-                       # public api
-                       url(r'/api/?', views.view_json),
-                       # paste submit (on site)
-                       url(r'/submit/?', views.submit_ajax),
-                       # paste replies
-                       url(r'/replies/?', views.view_replies),
-                       # latest pastes
-                       url(r'/latest/?', views.view_latest),
-                       # top pastes
-                       url(r'/top/?', views.view_top),
-                       # plain
-                       url(r'/raw/?', views.view_paste_raw),
-                       )
+urlpatterns = patterns(
+    '',
+    # paste main
+    url(r'^/?$', views.view_index),
+    # public api submit
+    url(r'/api/submit/?', views.submit_public),
+    # public api
+    url(r'/api/?', views.view_json),
+    # paste submit (on site)
+    url(r'/submit/?', views.submit_ajax),
+    # paste replies
+    url(r'/replies/?', views.view_replies),
+    # latest pastes
+    url(r'/latest/?', views.view_latest),
+    # top pastes
+    url(r'/top/?', views.view_top),
+    # plain
+    url(r'/raw/?', views.view_paste_raw),
+)
--- apps/urls.py
+++ apps/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 from apps import views as appviews
 
 # Patterns for apps.
-urlpatterns = patterns('',
-                       # apps index
-                       url(r'^/?$', appviews.view_index),
-                       # phonewords
-                       url(r'^/[Pp]hone[Ww]ords',
-                           include('apps.phonewords.urls')),
-                       # paste
-                       url(r'^/[Pp]aste',
-                           include('apps.paste.urls')),
-                       )
+urlpatterns = patterns(
+    '',
+    # apps index
+    url(r'^/?$', appviews.view_index),
+    # phonewords
+    url(r'^/[Pp]hone[Ww]ords',
+        include('apps.phonewords.urls')),
+    # paste
+    url(r'^/[Pp]aste',
+        include('apps.paste.urls')),
+)
