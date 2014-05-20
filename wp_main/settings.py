14c7f223b6605f8550b80428813294f0f444ea1c
Christopher Welborn <cj@welbornprod.com>
2014-05-19 22:01:15 -0500

    Bumped version after bug fixes.

--- wp_main/settings.py
+++ wp_main/settings.py
@@ -6,7 +6,7 @@
 #  2.0 is considered a new beginning because the project
 #  is moving towards a backward-incompatible state,
 #  where all python 2 'hacks' will be removed.)
-WPVERSION = '2.1.3'
+WPVERSION = '2.1.4'
 
 # file/path (path joining)
 import os.path
