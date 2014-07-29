e2798d4b705af6c3a529a2594a14b784d1925cf5
Christopher Welborn <cj@welbornprod.com>
2014-07-29 17:20:19 -0500

    Add latest tweet (a list of dicts) to home context.

--- home/views.py
+++ home/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.decorators import login_required
 from wp_main.utilities import utilities
 from wp_main.utilities import responses
 from wp_main.utilities import htmltools
+from wp_main.utilities import tweets
 from scripts import wpstats
 
 # logging
@@ -37,17 +38,19 @@ def convert_pblock(pblock):
 
 def index(request):
     """ serve up main page (home, index, landing) """
-
+    # Get latest tweet (if available.)
+    latesttweet = tweets.get_tweets('cjwelborn', count=1)
     # render main page
     context = {
         'request': request,
         'blog_post': htools.get_latest_blog(),
         'featured_project': htools.get_featured_project(),
         'featured_app': htools.get_featured_app(),
+        'latest_tweet': latesttweet,
         'extra_style_link_list': [utilities.get_browser_style(request)],
     }
     return responses.clean_response('home/index.html', context)
-    
+
 
 def makestats(name, getfunc, orderby=None):
     """ Create a StatsInfo() object from a name, get_info_func, and orderby
@@ -81,7 +84,7 @@ def view_500(request):
 
 def view_about(request):
     """ return the about page for welbornproductions. """
-    
+
     # Pass link list for the about page
     context = {
         'request': request,
@@ -97,7 +100,7 @@ def view_about(request):
 
 def view_badlogin(request):
     """ show the bad login message """
-    
+
     context = {
         'request': request,
         'extra_style_link_list': [utilities.get_browser_style(request)],
@@ -123,11 +126,11 @@ def view_debug(request):
 
 def view_error(request, error_number):
     """  returns  appropriate error page when given the error code. """
-    
+
     request_path = request.META['PATH_INFO']
     if request_path.startswith('/'):
         request_path = request_path[1:]
-    
+
     serror = str(error_number)
     # if its not one of these I don't have a template for it,
     # so it really would be a file-not-found error.
@@ -160,31 +163,31 @@ def view_ip_simple(request):
 
 def view_login(request):
     """ processes login attempts ## NOT BEING USED RIGHT NOW ##"""
-    
+
     # My first attempt at a login page, not very good.
     # I am using Django's auth.views with modified css right now.
     # maybe in the future I can do this right.
-    
+
     username = request.REQUEST.get('user', None)
     pw = request.REQUEST.get('pw', None)
-    
+
     #_log.debug("username: " + str(username) + ", pw: " + str(pw))
-    
+
     response = responses.redirect_response("/badlogin.html")
     if (username is not None) and (pw is not None):
-        
+
         user = auth.authenticate(user=username, password=pw)
-       
+
         #_log.debug("USER=" + str(user))
-        
+
         if user is not None:
             if user.is_active():
                 auth.login(request, user)
                 referer_view = responses.get_referer_view(request,
                                                           default=None)
-                
+
                 #_log.debug("referer_view: " + str(referer_view))
-                
+
                 # Change response based on whether or not prev. view was given.
                 if referer_view is None:
                     # Success
@@ -194,13 +197,13 @@ def view_login(request):
                     response = responses.redirect_response(referer_view)
 
     return response
-    
+
 
 def view_scriptkids(request):
-    """ return my script kiddie view 
+    """ return my script kiddie view
         (for people trying to access wordpress-login pages and stuff like that.)
     """
-    
+
     # get ip if possible.
     #ip_address = request.META.get("HTTP_X_FORWARDED_FOR", None)
     # if ip_address is None:
@@ -218,7 +221,7 @@ def view_scriptkids(request):
     if scriptkid_img is not None:
         scriptkid_img = utilities.get_relative_path(scriptkid_img)
     use_img = (scriptkid_img is not None)
-    
+
     context = {
         'request': request,
         'extra_style_link_list': [
@@ -238,7 +241,7 @@ def view_stats(request):
     """ return stats info for projects, blog posts, and file trackers.
         should require admin permissions.
     """
-    
+
     if request.user.is_authenticated():
         # gather print_block stats from wpstats and convert to lists of strings.
         # for projects, misc objects, blog posts, and file trackers...
