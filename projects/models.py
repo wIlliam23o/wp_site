4bbcf206159aa250d370e91842789a26ddbd77bc
Christopher Welborn <cj@welbornprod.com>
2015-01-12 15:42:52 -0600

    Cleanup, fix default date, fix get_latest_by.

--- projects/models.py
+++ projects/models.py
@@ -18,53 +18,90 @@ class wp_project(models.Model):
 
     """ Holds information about a single wp project. """
     # project name
-    name = models.CharField(blank=False, max_length=100,
-                            help_text="Name of the project. (Must be unique)")
+    name = models.CharField(
+        blank=False,
+        max_length=100,
+        help_text="Name of the project. (Must be unique)")
     # alias for acessing projects by name
     # ...should be lowercase-no-spaces project name
-    alias = models.CharField(blank=False, max_length=100,
-                             help_text="Alias for this project (lowercase, used to build urls)")
+    alias = models.CharField(
+        blank=False,
+        max_length=100,
+        help_text="Alias for this project (lowercase, used to build urls)")
     # short description of project
     # (longer description should be in it's html file)
-    description = models.CharField(blank=True, default="", max_length=1024,
-                                   help_text="Short description for the project.")
+    description = models.CharField(
+        blank=True,
+        default='',
+        max_length=1024,
+        help_text="Short description for the project.")
     # version in the form of X.X.X or XX.X.X
-    version = models.CharField(blank=True, default="1.0.0", max_length=120,
-                               help_text="Version string in the form of X.X.X")
+    version = models.CharField(
+        blank=True,
+        default="1.0.0",
+        max_length=120,
+        help_text="Version string in the form of X.X.X")
     # url to image file (will be embedded in html)
-    logo_url = models.CharField(blank=True, default="", max_length=512,
-                                help_text="URL of logo image (possible future development.)")
+    logo_url = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text="URL of logo image (possible future development.)")
     # html file for long description
-    html_url = models.CharField(blank=True, default="", max_length=512,
-                                help_text="Html file for this project, containing the description.")
+    html_url = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text="Html file for this project, containing the description.")
     # url to package for download (/files/project-version.tar.gz)
-    download_url = models.CharField(blank=True, default="", max_length=512,
-                                    help_text="Location of main file to download for the project. (Can be a directory)")
+    download_url = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text=(
+            "Location of main file to download for the project. "
+            "(Can be a directory)"))
     # directory for screenshot images
-    screenshot_dir = models.CharField(blank=True, default="", max_length=512,
-                                      help_text="Directory containing screenshots for the project (Relative location)")
+    screenshot_dir = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text=(
+            "Directory containing screenshots for the project "
+            "(Relative location)"))
     # optional main source file (.py file to view)
-    source_file = models.CharField(blank=True, default="", max_length=512,
-                                   help_text="Main source code file for the project for viewing.")
+    source_file = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text="Main source code file for the project for viewing.")
     # optional source dir (dir containing source files to view)
-    source_dir = models.CharField(blank=True, default="", max_length=512,
-                                  help_text="Directory containing multiple source code files for viewing.")
+    source_dir = models.CharField(
+        blank=True,
+        default="",
+        max_length=512,
+        help_text=(
+            "Directory containing multiple source code files for viewing."))
     # publish date (for sort-order mainly)
-    publish_date = models.DateField(blank=False, default=get_date_today(),
-                                    help_text="Date the project was published. (Automatically set to today.)")
+    publish_date = models.DateField(
+        blank=False,
+        default=get_date_today,
+        help_text=(
+            "Date the project was published. (Automatically set to today.)"))
 
     # disables project (instead of deleting it, it simply won't be viewed)
     disabled = models.BooleanField(default=False)
 
     # count of views/downloads
-    view_count = models.IntegerField(default=0,
-                                     help_text="How many times this project has been viewed. (Integer)")
-    download_count = models.IntegerField(default=0,
-                                         help_text="How many times this project has been downloaded. (Integer)")
+    view_count = models.IntegerField(
+        default=0,
+        help_text="How many times this project has been viewed. (Integer)")
+    download_count = models.IntegerField(
+        default=0,
+        help_text="How many times this project has been downloaded. (Integer)")
 
     # admin stuff
     date_hierarchy = 'publish_date'
-    get_latest_by = 'publish_date'
 
     def __unicode__(self):
         """ returns string/unicode representation of project """
@@ -83,6 +120,7 @@ class wp_project(models.Model):
 
     # Meta info for the admin site
     class Meta:
+        get_latest_by = 'publish_date'
         ordering = ['name']
         verbose_name = "Project"
         verbose_name_plural = "Projects"
