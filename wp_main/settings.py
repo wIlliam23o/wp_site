e7fe4e26f38fe701c68f159f271e2fb39558b4de
Christopher Welborn <cj@welbornprod.com>
2015-01-25 22:26:28 -0600

    Moving to more sane secret settings.

--- wp_main/settings.py
+++ wp_main/settings.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # Version for welbornprod.com
-# (versioning didn't start until the move to python3,
-#  2.0 is considered a new beginning because the project
-#  is moving towards a backward-incompatible state,
-#  where all python 2 'hacks' will be removed.)
-WPVERSION = '2.1.6'
+WPVERSION = '2.1.7'
 
 # file/path (path joining)
 import os.path
 import sys
 SYSVERSION = sys.version
 
+import settings_local
+TEMPLATE_DEBUG, DEBUG = settings_local.TEMPLATE_DEBUG, settings_local.DEBUG
+
+
 # Django messages framework, message-levels
-#from django.contrib.messages import constants as message_constants
-#MESSAGE_LEVEL = message_constants.ERROR
+# from django.contrib.messages import constants as message_constants
+# MESSAGE_LEVEL = message_constants.ERROR
 
 # DEBUG is in settings_local...
 
@@ -26,111 +26,31 @@ SCRIPT_PARENT = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.split(SCRIPT_PARENT)[0]
 # get parent of application
 BASE_PARENT = os.path.split(BASE_DIR)[0]
-
 # File for list of IP's to ban.
 SECRET_BAN_FILE = os.path.join(BASE_DIR, 'wp_banned.lst')
-
-# test or live?
-if 'webapps' in BASE_PARENT:
-    # live site directories
-    STATIC_PARENT = BASE_PARENT
-    # Static dirs based on test/live site. Set in local_settings.
-    STATIC_ROOT = 'unknown'
-    MEDIA_URL = 'http://welbornprod.com/media/'
-    SERVER_LOCATION = 'remote'
-else:
-    # local development directories
-    STATIC_PARENT = '/var/www/'
-    STATIC_ROOT = '/var/www/static'
-    MEDIA_URL = 'http://127.0.0.1/media/'
-    SERVER_LOCATION = 'local'
-
+# Load secretive settings.
+SECRETS = settings_local.SecretSettings(BASE_DIR)
+# Server/Site info.
+SECRET_KEY = SECRETS.settings['secret_key']
+SITE_URL = SECRETS.site_url
+SITE_VERSION = SECRETS.site_info['site_version']
+SERVER_LOCATION = SECRETS.site_info['location']
 
 # Static/Media directories.
+STATIC_PARENT, STATIC_ROOT = SECRETS.static_parent, SECRETS.static_root
+STATIC_URL = SECRETS.static_url
+MEDIA_URL = SECRETS.site_info['media_url']
 MEDIA_ROOT = os.path.join(STATIC_PARENT, "media")
 
-# main app (location of settings.py)
-MAIN_DIR = os.path.join(BASE_DIR, "wp_main")
-TEMPLATES_BASE = os.path.join(MAIN_DIR, "templates")
-
-# IP's debug_toolbar should be shown to.
-# check for local internal_ips.txt,
-# allow the ips in that file if it exists.
-_internal_ips = ['127.0.0.1']
-# KEY is in the variable name so django debug automatically
-# hides it from debug.
-KEY_INTERNAL_IPS_FILE = os.path.join(BASE_DIR, "internal_ips.txt")
-if os.path.isfile(KEY_INTERNAL_IPS_FILE):
-    try:
-        with open(KEY_INTERNAL_IPS_FILE) as f_ips:
-            ip_raw = f_ips.read()
-            if '\n' in ip_raw:
-                # list of ips in file.
-                ips_list = []
-                for ip_ in ip_raw.split('\n'):
-                    ip_ = ip_.strip()
-                    if len(ip_) > 7 and (not ip_.startswith('#')):
-                        _internal_ips.append(ip_)
-            else:
-                # single ip in file
-                _internal_ips.append(ip_raw)
-    except Exception as ex:
-        pass
-
-# Add local dev ips to safe list.
-_internal_ips.extend(['192.168.0.{}'.format(n) for n in range(2, 21)])
-_internal_ips.extend(['192.168.1.{}'.format(n) for n in range(2, 21)])
-# set global allowed ips
-INTERNAL_IPS = tuple(_internal_ips)
-
-
-# Admin info
-ADMINS = (('Christopher Welborn', 'cj@welbornprod.com'), )
-MANAGERS = ADMINS
-
+# Email info.
+EMAIL_HOST = SECRETS.settings['email']['host']
+EMAIL_HOST_USER = SECRETS.settings['email']['user']
+EMAIL_HOST_PASSWORD = SECRETS.email_pw
+EMAIL_FROM_EMAIL = SECRETS.settings['email']['from_email']
+SERVER_EMAIL = SECRETS.settings['email']['email']
 
 # Database info (filled in with settings_local)
-DATABASES = {
-    'default': {
-        'ENGINE': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-#------------------ Settings above this may be squashed by settings_local -----
-# Fill in missing settings from local file (not in git).
-SECRET_LOCAL_SETTINGS = os.path.join(BASE_DIR, 'settings_local.py')
-# This is a hack. Badly recommended from several places on the internet.
-# This could probably be done better with a secret JSON file, or even a
-# sqlite database. The idea would be the same but it would only parse/read
-# the data, and no code would be executed.
-#     like: json.loads(open(secret_file).read())
-# The only problem is the few 'decisions' that the local-settings-file makes.
-# ...like setting 'SITE_VERSION' based on cwd. (harmless debug info but still)
-if sys.version_info.major < 3:
-    try:
-        execfile(SECRET_LOCAL_SETTINGS)  # noqa
-    except Exception as ex:
-        sys.stderr.write('\n'.join([
-            'Error including settings_local.py!',
-            'This will not work.',
-            '{}\n'.format(ex)]))
-else:
-    # Python 3 exec file is gone.
-    try:
-        exec(compile(open(SECRET_LOCAL_SETTINGS).read(),
-                     SECRET_LOCAL_SETTINGS,
-                     'exec'),
-             globals(), locals())
-    except Exception as ex:
-        sys.stderr.write('\n'.join([
-            'Error including settings_local.py!',
-            'This will not work.',
-            '{}\n'.format(ex)]))
+DATABASES = {'default': SECRETS.database}
 
 # Cache Settings
 CACHES = {
@@ -144,12 +64,29 @@ CACHES = {
     }
 }
 # Set which cache to use.
-if 'webapps' in BASE_PARENT and (not 'test' in BASE_PARENT):
+if 'webapps' in BASE_PARENT and ('test' not in BASE_PARENT):
     # Use db cache for live site.
     CACHES['default'] = CACHES['cache_db']
 else:
     # Use dummy cache for local development, and test site.
     CACHES['default'] = CACHES['cache_dummy']
+
+# main app (location of settings.py)
+MAIN_DIR = os.path.join(BASE_DIR, "wp_main")
+TEMPLATES_BASE = os.path.join(MAIN_DIR, "templates")
+
+# IP's debug_toolbar should be shown to.
+_internal_ips = SECRETS.settings['internal_ips']
+# Add local dev ips to safe list.
+_internal_ips.extend(['192.168.0.{}'.format(n) for n in range(2, 21)])
+_internal_ips.extend(['192.168.1.{}'.format(n) for n in range(2, 21)])
+# set global allowed ips
+INTERNAL_IPS = tuple(_internal_ips)
+
+
+# Admin info
+ADMINS = (('Christopher Welborn', 'cj@welbornprod.com'), )
+MANAGERS = ADMINS
 
 
 # Local time zone for this installation. Choices can be found here:
@@ -189,20 +126,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# SECRET_KEY SETTINGS
-# Make this unique, and don't share it with anybody.
-SECRET_KEY_FILE = os.path.join(BASE_DIR, "secretkey.txt")
-if os.path.isfile(SECRET_KEY_FILE):
-    try:
-        with open(SECRET_KEY_FILE) as fread:
-            SECRET_KEY = fread.read().replace('\n', '')
-    except (IOError, OSError)as ex_access:
-        # failed to read secretkey.txt
-        SECRET_KEY = NONSECRET_KEY  # noqa
-else:
-    # no secret key exists.
-    SECRET_KEY = NONSECRET_KEY  # noqa
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
