a20d2d00153c12353432ab04cb3ac3e5f3684607
Christopher Welborn <cj@welbornprod.com>
2015-05-04 12:19:43 -0500

    Timestamp change.

--- wp_main/robots/robots.py
+++ wp_main/robots/robots.py
@@ -4,10 +4,10 @@
 '''
       project: welborn productions - robots - main
      @summary: provide a robots.txt file
-    
+
       @author: Christopher Welborn <cj@welbornproductions.net>
 @organization: welborn productions <welbornproductions.net>
- 
+
    start date: Apr 9, 2013
 '''
 from django.views.decorators.cache import never_cache
@@ -17,15 +17,15 @@ from wp_main.utilities import responses
 @never_cache
 def view_robots_blockall(request):
     """ block all pages """
-    
-    return responses.text_response("User-agent: *\nDisallow: /")
+
+    return responses.text_response('User-agent: *\nDisallow: /')
 
 
 @never_cache
 def view_robots_any(request):
     """ allow any page """
-    
-    return responses.text_response("User-agent: *\nAllow: /")
+
+    return responses.text_response('User-agent: *\nAllow: /')
 
 
 @never_cache
@@ -33,10 +33,10 @@ def view_byserver(request):
     """ decide which robots.txt to serve up based on current server.
         currently blocks all for any servername starting with 'test.'
     """
-    
-    server_name = request.META["SERVER_NAME"]
-    
-    if server_name.startswith("test."):
+
+    server_name = request.META['SERVER_NAME']
+
+    if server_name.startswith('test.'):
         return view_robots_blockall(request)
     else:
         # normal robots, allow all.
