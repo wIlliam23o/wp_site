fa4390426a7f43a5b31a31cb502f7412b2f742a8
Christopher Welborn <cj@welbornprod.com>
2015-07-20 16:23:37 -0500

    Fix new pygments formatter style.

--- wp_main/utilities/highlighter.py
+++ wp_main/utilities/highlighter.py
@@ -45,19 +45,27 @@ STYLECODES = {'b': '<span class=\'B\'>{}</span>',
 
 # Aliases for pygments lexer names. These are switched to the long name
 # before highlighting.
-STYLEALIASES = {'py': 'python',
-                'cmdopt': 'cmdoption',
-                'cmdval': 'cmdvalue',
-                'copt': 'cmdoption',
-                'cval': 'cmdvalue',
-                }
+STYLEALIASES = {
+    'py': 'python',
+    'cmdopt': 'cmdoption',
+    'cmdval': 'cmdvalue',
+    'copt': 'cmdoption',
+    'cval': 'cmdvalue',
+}
 STYLENAMES = list(STYLECODES.keys())
 
 # Preload the default formatter.
-DEFAULT_FORMATTER = formatters.html.HtmlFormatter(
-    linenos=False,
-    nowrap=True,
-    style='default')
+try:
+    DEFAULT_FORMATTER = formatters.html.HtmlFormatter(
+        linenos=False,
+        nowrap=True,
+        style='default')
+except AttributeError:
+    # Pygments 2.0.2+
+    DEFAULT_FORMATTER = formatters.get_formatter_by_name('html')
+    DEFAULT_FORMATTER.linenos = False
+    DEFAULT_FORMATTER.nowrap = True
+    DEFAULT_FORMATTER.style = 'default'
 
 
 class WpHighlighter(object):
