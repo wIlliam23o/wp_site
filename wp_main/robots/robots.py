8c6f85ee771b9443cdfbb14acfe9f422f9b5c2be
Christopher Welborn <cj@welbornprod.com>
2016-01-11 22:08:16 -0600

    Disallow some urls in robots.txt (easily-configurable).

--- wp_main/robots/robots.py
+++ wp_main/robots/robots.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-      project: welborn productions - robots - main
-     @summary: provide a robots.txt file
-
-      @author: Christopher Welborn <cj@welbornproductions.net>
-@organization: welborn productions <welbornproductions.net>
-
-   start date: Apr 9, 2013
+''' Welborn Productions - Robots
+    Serves a robots.txt file.
+    -Christopher Welborn 4-9-13
 '''
 from django.views.decorators.cache import never_cache
 from wp_main.utilities import responses
+
+disallowed = {
+    '/apps/timekeeper/',
+}
+disallowed_template = """
+User-agent: *
+{config}
+"""
 
 robots_any = """
 User-agent: *
@@ -33,6 +36,14 @@ def view_robots_blockall(request):
 @never_cache
 def view_robots_any(request):
     """ Allow any page """
+    if disallowed:
+        # Only allow pages not in disallowed.
+        return responses.text_response(
+            disallowed_template.format(
+                config='\n'.join('Disallow: {}'.format(s) for s in disallowed)
+            )
+        )
+    # Don't block any pages.
     return responses.text_response(robots_any)
 
 
