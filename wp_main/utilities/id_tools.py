53606b1b3e6c7510ebb932a32c4930a3ba84a49d
Christopher Welborn <cj@welbornprod.com>
2017-01-03 22:31:22 -0600

    Use a more random looking id generation.

--- wp_main/utilities/id_tools.py
+++ wp_main/utilities/id_tools.py
@@ -5,28 +5,55 @@
 """
 from random import SystemRandom
 
-IDCHOICES = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 SYSRANDOM = SystemRandom()
+# Changing the start char will wreck previously encoded ids.
 IDSTARTCHAR = 'a'
-IDPADCHARS = ('x', 'y', 'z')
+# IDPADCHARS cannot contain IDSTARTCHAR + 9 letters (a -> j, when 'a' is used)
+IDPADCHARS = 'lmnopqrstuvwxyz'
 
 
-def generate_random_id(length=4):
-    """ Generate a random/unique id. """
-    finalid = [SYSRANDOM.choice(IDCHOICES) for i in range(length)]
-    return ''.join(finalid)
+def encode_id(realid, length=4):
+    """ A form of encoding that is reversible.
+        Ensures an id at least `length` + `length // 2` characters long.
+        Example:
+            encode_id(123, length=4)
+            'btycydx'
 
+        Arguments:
+            realid           : Int or digit string to encode.
+            length           : Max length for the actual id.
+                               Automatically bumped up to len(str(realid))
+                               when needed.
+    """
+    realidstr = str(realid)
+    if not realidstr.isdigit():
+        raise ValueError(
+            'Expecting a number, or str of digits. Got: ({}) {!r}'.format(
+                type(realid).__name__,
+                realid,
+            )
+        )
+    realidlen = len(realidstr)
+    if realidlen > length:
+        # Bump up the length.
+        length = realidlen
 
-def encode_id(realid):
-    """ A form of encoding that is reversible. """
     startchar = ord(IDSTARTCHAR)
-
     finalid = []
-    for c in str(realid):
-        newchar = chr(startchar + int(c))
+    for digit in realidstr:
+        newchar = chr(startchar + int(digit))
         finalid.append(newchar)
-    while len(finalid) < 4:
+
+    # Do the actual 'encoding' part.
+    while len(finalid) < length:
         finalid.append(SYSRANDOM.choice(IDPADCHARS))
+
+    # Insert random padding characters in random places.
+    for _ in range(length // 2):
+        finalid.insert(
+            SYSRANDOM.randint(0, length - 1),
+            SYSRANDOM.choice(IDPADCHARS)
+        )
     return ''.join(finalid)
 
 
